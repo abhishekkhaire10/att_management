d43dd6a9ab87953a8ec9f886e2f137e890d9a6a0
Abhishek Khaire <Bornmercurial.ak@gmail.com>
2021-04-15 17:22:27 +0530

    Updated host and port

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ def logout():
 #     return render_template('student_homepage.html', name = name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = '8080')
